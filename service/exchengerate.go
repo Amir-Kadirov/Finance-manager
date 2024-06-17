fab8c1e9cebf8017692884a34f8850f6bcf130bd
Amir <amirjonqodirov28@gmail.com>
2024-06-17 16:44:48 +0500

    fixed bugs

--- service/exchengerate.go
+++ service/exchengerate.go
@@ -7,6 +7,7 @@ import (
 	"finance/storage"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type currencyService struct {
@@ -25,26 +26,28 @@ type ExchangeRateAPIResponse struct {
 }
 
 func (c currencyService) GetRate(ctx context.Context,apiKey string,req models.ExchangeRateRequest) (string, error) {
+    req.To=strings.ToUpper(req.To)
+    req.From=strings.ToUpper(req.From)
     url :=fmt.Sprintf("https://api.currencyapi.com/v3/latest?apikey=%s&currencies=%s&base_currency=%s", apiKey,req.To,req.From)
 
     resp, err := http.Get(url)
     if err != nil {
-        return "0", fmt.Errorf("error making HTTP request: %w", err)
+        return "", fmt.Errorf("error making HTTP request: %w", err)
     }
     defer resp.Body.Close()
 
     if resp.StatusCode != http.StatusOK {
-        return "0", fmt.Errorf("received response code: %d", resp.StatusCode)
+        return "", fmt.Errorf("received response code: %d", resp.StatusCode)
     }
 
     var response ExchangeRateAPIResponse
     if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-        return "0", fmt.Errorf("error decoding JSON response: %w", err)
+        return "", err
     }
 
     currencyRate, ok := response.Data[req.To]
     if !ok {
-        return "0", fmt.Errorf("rate not found in response")
+        return "", fmt.Errorf("rate not found in response")
     }
 
     if req.Amount==0 {
